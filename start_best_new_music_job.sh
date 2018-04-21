0fcea9fc665b617d72e2d60f539c18d9e8b7a5ee
Matthew Faw <matthewfaw@gmail.com>
2018-04-21 03:55:20 -0400

    Create objects in namespace, and assign serviceAccount to job so it can run in the k8s cluster created by kops

--- start_best_new_music_job.sh
+++ start_best_new_music_job.sh
@@ -9,6 +9,8 @@ if [ -z $SLACK_BEST_NEW_MUSIC_WEBHOOK ]; then
     exit 1
 fi
 
+kubectl apply -f k8s/namespace.yaml
+
 scripts_to_populate=("fetch-recent-reviews.py" "filter-already-seen.py" "create-slack-message.py")
 
 if [ "$SETTINGS" = "with_resources" ]; then
